c06fda3e3be66a86661633a438c8f8ab25ed8ef4
Lenin Ugalde <ugaldelenin@hotmail.com>
2023-07-09 20:13:34 -0600

    moka project

---
+++ js/header.js
@@ -0,0 +1,48 @@
+
+const dropdownBtn = document.querySelectorAll(".dropdown-btn");
+const dropdown = document.querySelectorAll(".dropdown");
+const hamburgerBtn = document.getElementById("hamburger");
+const navMenu = document.querySelector(".menu");
+const links = document.querySelectorAll(".dropdown a");
+
+
+function setAriaExpandedFalse() {
+    dropdownBtn.forEach((btn) => btn.setAttribute("aria-expanded", "false"));
+}
+
+function closeDropdownMenu() {
+    dropdown.forEach((drop) => {
+        drop.classList.remove("active");
+        drop.addEventListener("click", (e) => e.stopPropagation());
+    });
+}
+
+function toggleHamburger() {
+    navMenu.classList.toggle("show");
+}
+
+// close dropdown menu when the dropdown links are clicked
+links.forEach((link) =>
+    link.addEventListener("click", () => {
+        closeDropdownMenu();
+        setAriaExpandedFalse();
+        toggleHamburger();
+    })
+);
+
+// close dropdown menu when you click on the document body
+document.documentElement.addEventListener("click", () => {
+    closeDropdownMenu();
+    setAriaExpandedFalse();
+});
+
+// close dropdown when the escape key is pressed
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        closeDropdownMenu();
+        setAriaExpandedFalse();
+    }
+});
+
+// toggle hamburger menu
+hamburgerBtn.addEventListener("click", toggleHamburger);
